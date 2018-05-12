68252964f0060f56a078fde3814744afe7fa237b
miguel <mrr466@inlumine.ual.es>
2018-05-12 17:16:22 +0200

    mayo
    mejora del reloj

--- codigo/pluviometro.ino
+++ codigo/pluviometro.ino
@@ -1,83 +1,169 @@
-#include <ESP8266WiFi.h>//libreria para el deep-sleep, bajo consumo
+#include <Wire.h>  // Incluye la libreria Wire
+#include <RTClib.h> // Incluye la libreria RTClib
 #include <SD.h>// libreria para el manejo de ficheros
-#include <SPI.h>// libreria para comunicacion SPI
-#include <RTClib.h>//libreria para el control del reloj(RTC)
+#define pin D0
 
-//declaracion de variables globales//
-
-const int chipSelect = D8;//??
-RTC_DS1307 RTC;//Crea el objeto RTC
+RTC_DS1307 RTC; // Crea el objeto RTC
 DateTime now;//Obtiene la fecha y la hora del RTC
-//Me ne frega.
-  int State=0;
-//TIME
-    int nHour,nMin,nSec = 1;
-    boolean alarma1 = true;
 
-//DATE
-    int nDay,nMonth,nYear = 1;
+//Declaramos las variables que utilizaremos durante la ejecucón
+const int chipSelect = D8;//CS de la SD
+int nHour,nMin,nSec = 1;
+boolean alarma1 = true;
+int nDay,nMonth,nYear = 1;
+String file,logN,ruta = "";
+char fecha[18];
+int tiempo[6];
+bool garfagnana= true;
+int opcion=0;
+
+//Está es la función principal que se ejecuta.
+void setup () {
+  
+  pinMode(pin,INPUT);
+  Serial.begin(9600); // Establece la velocidad de datos del puerto serie
+  Wire.begin(); // Establece la velocidad de datos del bus I2C
+  RTC.begin(); // Establece la velocidad de datos del RTC
+  SD.begin(chipSelect);
+  
+   // He tomado un ejemplo de la pagina de Arduino, de tal forma que con el goto
+   //Se anula la opción de poner en reposo el microcontrolador saltando toda esa parte
+   // Por defecto solo se ejecuta setup(), entra en reposo y cuando despierta ejecuta de nuevo setup ()
    
-//FILES
-  String folder,file,logN,ruta = "";
-void setup() {
-//  Serial.begin(9600);
-
- WiFi.forceSleepBegin();
-  //pinMode(lectura,INPUT);  //Declaramos el Pin para el swicht magnetico
- // pinMode(LED_BUILTIN, OUTPUT);//Led del WEMOS Parpadeo si swicht on
-  /*
-  Serial.begin(9600);//PUERTO serial
-  Serial.println("estoy despierto");
-  /////**SD**
-  Serial.print(F("Iniciando SD ..."));
-  if (!SD.begin(chipSelect))
-  {
-  Serial.println(F("Error al iniciar"));
-   return;
-  }
-*/
-SD.begin(chipSelect);
-  //inizialimaos el reloj
-  RTC.begin();  
-  //delay (100);
- // digitalWrite(LED_BUILTIN, LOW);  
-  //Serial.println(F("Iniciado correctamente"));
+  //ESP.deepSleep(0);
+  if(digitalRead(pin)==HIGH)goto bailout;//pin es para pasar al modo configuración del microcontrolador
   obtenerHora();
   generarRuta();
-  guardarArchivo();   
-  
-  //Serial.println("vamos a dormir");
-  
-//MODO dormir para siempre (0); despierta enviando a tierra "rst"
-//el balancin hace la funcion de swicht, cada movimiento provoca 
-//que despierte del reposo, anote los datos y vuelve al reposo.
-if(nDay==1)recuento();
-ESP.deepSleep(0);
+  guardarArchivo(); 
+  ESP.deepSleep(0);//envia en reposo al dispositivo
+  bailout:;
 }
+/* *********************************************************
+ * Loop () se ejecuta por defecto siempre de forma          *  
+ * salvo en este caso con la función deepsleep solo entrara *
+ * en el caso de estar activado el botón de                 *
+ * configuración.                                           *
+ ***********************************************************/
+void loop () {
 
-void loop() {
-}
 
- /***************************************************
-*FUNCION RUTA: Creamos la ruta con formato:         *
-*       folder --> /ano.mes                         *
-*       file   -->/ano.mes.dia.csv                  *
-*       LogN   -->ano-mes-dia-,hora:minutos:segundos*
-*       ruta   -->/ano.mes/ano.mes.dia.csv          *
- ***************************************************/
-void generarRuta(){
-  //Serial.println("generando ruta");
-      folder ="/" + String(nYear) + String(nMonth);
-      file ="/"+ String(nYear)  + String(nMonth) + String(nDay) + ".csv";// fecha+extension
-      logN = String(nYear) + "-" + String(nMonth) + "-" + String(nDay) + "," //fecha
-                      + String(nHour) + ":" + String(nMin) + ":" 
-                      + String(nSec) ; //hora;
-      ruta = folder + file;
+  if(digitalRead(pin)==LOW)ESP.deepSleep(0);
+  // si no esta activo el botón, quiere decir que hemos acabado con la configuración y lo envía en reposo.
+
+  switch(opcion)
+  {
+    //garfagnana lo utilizo para saber cuando pasa el programa por la función.
+    
+    case 1:
+    garfagnana=true;
+    if (Serial.available() > 0)//Si hay datos en la entrada se entra en el if
+    {
+      agiorna ();// función para actualizar la fecha 
+      garfagnana=true;
+      opcion=0;//Para poner el marcador a 0 y llamar el caso default.
+      break;
+    }else {opcion=1;
+     if (garfagnana==false)Serial.println("formato xx-xx-xx-xx-xx, quiere decir Anno-mes-dia-hora-min dos cifras!!");
+    garfagnana=true;
+    break;
+    }
+    
+    case 2:
+    leerymostrar();//Para mostrar por pantalla toda la información almacenada en la tarjeta de memoria
+    opcion=0;
+    garfagnana=true;
+    break;
+    case 3:
+    //El caso 3 es un tester para controlar que todo es correcto.
+     obtenerHora();
+     generarRuta();
+     Serial.println();
+     Serial.println(logN);
+     Serial.println();
+     opcion=0;
+     garfagnana=true;
+    break;          
+    case 4:
+    //Caso 4: Es borrar la tarjeta de memoria
+        opcion=0;
+        borrar();
+        garfagnana=true;
+         break;         
+    case 5:
+    //Para acabar y dejar en reposo
+      opcion=0;
+      ESP.deepSleep(0);
+    break;  
+    default:
+    if(garfagnana)
+    {
+      garfagnana=false;
+      ordenes();
+      
+    }
+    
+    if(Serial.available()>0){
+    opcion=Serial.parseInt();//Almacena la opción tomada
+    //Serial.print("la opcion es = ");
+    //Serial.println(opcion);
+    }
+    break;
   }
-/*********************************************
+
+}
+
+void agiorna ()
+{
+    Serial.println("cadena aceptada");
+    (Serial.readString()).toCharArray(&fecha[0],18) ;
+    Serial.println(fecha);
+    //El siguinete bucle  es para transformar la String en un entero, 
+    //la información contenida en el String se encuentra en ASCII y se debe convertir
+    for(int i=0;i<5;i++)
+    {
+      tiempo[i]=10*(fecha[3*i]-'0')+fecha[3*i+1]-'0';
+      Serial.println(tiempo[i]) ;
+    }
+/*
+ * va esperar el siguiente formato xx-xx-xx-xx-xx, quiere decir Anno-mes-dia-hora-min dos cifras!!
+ * tiempo[0]:anno
+ * tiempo[2]:mes
+ * tiempo[3]:dia
+ * tiempo[4]:min
+ * 0: Segundos
+*/
+    RTC.adjust(DateTime(tiempo[0], tiempo[1],tiempo[2], tiempo[3], tiempo[4], 0));
+
+}
+ /**********************************************
+ * Guarda el archivo de los datos recuperados  * 
+ * del reloj, creando una carpeta, y dentro de *
+ * esa carpeta aloja el archivo de ese dia de  * 
+ * los eventos sucedidos, un dia una hoja.     *
+ **********************************************/
+ 
+ void guardarArchivo(){
+  File f;      
+      Serial.println(ruta);
+      Serial.println(logN);
+      f = SD.open(ruta, FILE_WRITE);//abrimos  el archivo   
+    
+          if (f) 
+      { 
+       
+            Serial.println("escrito");              
+            f.println(logN);
+            Serial.println("escribimos el log");
+            f.close(); //cerramos el archivo  
+               
+      }    
+  }
+
+ /*********************************************
 * Funcion acceder al RTC, obtenemos la fecha *
 * a la que se producira el evento            *
 *********************************************/
+
 void obtenerHora(){
     //Serial.println("obteniendo hora");
     now = RTC.now();// obtiene la fecha y la hora del RTC
@@ -88,104 +174,68 @@ void obtenerHora(){
     nMonth = now.month();
     nYear = now.year();
   }
- /**********************************************
- * Guarda el archivo de los datos recuperados  * 
- * del reloj, creando una carpeta, y dentro de *
- * esa carpeta aloja el archivo de ese dia de  * 
- * los eventos sucedidos, un dia una hoja.     *
- **********************************************/
- void guardarArchivo(){
+
+/***************************************************
+*FUNCION RUTA: Creamos la ruta con formato:         *
+*       file   -->/ano.mes.dia.csv                  *
+*       LogN   -->ano-mes-dia-,hora:minutos:segundos*
+*       ruta   -->/ano.mes/ano.mes.dia.csv          *
+ ***************************************************/
+void generarRuta(){
+  //Serial.println("generando ruta");
+    float v;
+      v=analogRead(A0)*5.0/523; 
+      
+      ruta =String(nMonth) + ".csv";// fecha+extension
+      logN = String(nDay)+"/"+ String(nMonth)+"/"+String(nYear) +  "," //fecha
+                      + String(nHour) + ":" + String(nMin) + ":" 
+                      + String(nSec)+ ","+String(v); //hora;
+  }
+
+/* ************************************************
+ * Funcion LEER LECTURAS,                         *
+ * extrae la información de la tarjeta de memoria * 
+ *************************************************/
+ 
+void leerymostrar()
+{
+  String lectura;
   File f;
 
-//      Serial.println("");
-//      Serial.println(folder);
-//      Serial.println(ruta);
-//     Serial.println(logN);
-      char fichero[10];
-      char dir[50];
-      folder.toCharArray(fichero,10);
-      ruta.toCharArray(dir,50);      
-      //SD.mkdir(fichero);
-      if(!SD.exists(fichero)){
-        //Serial.println("creando carpeta");
-          SD.mkdir(fichero);
-      }
-      f = SD.open(dir, FILE_WRITE);//abrimos  el archivo
-//      Serial.println("ok");
-//      Serial.println(dir);
-          if (f) { 
-//            Serial.println("escrito");              
-            f.println(logN);
-//            Serial.println("escribimos el log");
-            f.close(); //cerramos el archivo  
-               
-      }    
-  }
- 
- /********************************************** ****************
- * Esta funcion revisa todos los datos almacenados en un mes,   *   
- * realiza un recuento, y crea un nuevo archivo que consta      *
- * el mes y la cantidad registrada                              *
- ****************************************************************/
- 
-  void recuento(){
-  float suma = 0;
-  String mese;
-  File l;
-  int i,k=0;//contador
-  //La funcion SD.open("",.. ) hay que mandarle un char
-      char dirr[50];  
-  if(nMonth==1){//Debemos de descartar el mes 0.
-    mese=12;
-  }else{mese=nMonth-1;}
-  for (i=1;i<=31;i++){//realizamos un barrido de todos los dias
-    ruta ="/" + String(nYear) + 
-    String(mese)
-    +"/"+ String(nYear)  + String(mese) +"-" + String(i) + ".csv";
-    ruta.toCharArray(dirr,25); 
-
-////
-if(SD.exists(dirr)){// Como hemos realizado el barrido debemos descartar los que no existan
-
-    l=SD.open(dirr,FILE_READ);
-//    Serial.println("hemos leido ");
-    if(l){
-//       Serial.println("l es verdad");
-        while(l.available()){
-        /*if( l.peek() == '\n' ){
-          k++;//Buscamos el indicador de salto de linea
-      Serial.println(folder);
-      Serial.println(ruta);
-          }*/
-          if(l.read()=='\n')k++;
-          }
-      l.close();
-      suma=suma+k*0.44853;//El valor numérico debe ser la relacion del volumen del balancin, y tamano de embudo
-      }
-
-   }    
+  for(int i=0;i<=11;i++)
+    {
+      ruta = String(i+1) + ".csv";// fecha+extension
+      if( f=SD.open(ruta)){
+        
+       while(f.available())Serial.write(f.read());
+       
+      }//else Serial.println("falso");
     }
-    ruta="/"+ String(nYear) + ".csv" ;
-    ruta.toCharArray(dirr,25);  
-    //A continuacion iniciamos la traduccion del numero a letra, 1->Enero, 2->Febr..
-    if(mese=1)mese = "Enero";
-    if(mese=2)mese = "Febrero";
-    if(mese=3)mese = "Marzo";
-    if(mese=4)mese = "Abril";
-    if(mese=5)mese = "Mayo";
-    if(mese=6)mese = "Junio";
-    if(mese=7)mese = "Julio";
-    if(mese=8)mese = "Agosto";
-    if(mese=9)mese = "Septiembre";
-    if(mese=10)mese = "Octubre";
-    if(mese=11)mese = "Noviembre";
-    if(mese=12)mese = "Diciembre";
-
-    if(l){
-      l.println(mese+","+String(suma)+","+"mm");
-//      Serial.println("hecho");
-    }
-    l.close();
+    Serial.println("fin");
+}
+void ordenes(){
+  Serial.println("opciones disponibles");
+  Serial.println("1-Actualizar fecha");
+  Serial.println("2-descargar datos");
+  Serial.println("3-Dame la fecha");
+  Serial.println("4-FormatearSD");
+  Serial.println("5-acabar");
   
 }
 
+/* ************************************************
+ * Funcion Borrar ,                               *
+ * Borra toda la información contenida en la      *
+ * tarjeta de memoria                             * 
+ *************************************************/
+void borrar()
+{
+    File f;
+
+  for(int i=0;i<=11;i++)
+    {
+      ruta = String(i+1) + ".csv";// fecha+extension
+      if( f=SD.open(ruta)) SD.remove(ruta);  
+    }  
+
+}
